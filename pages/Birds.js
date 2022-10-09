a3f2ae4df0645532212a252bb59b5744128fca8a
Zhou Yuxin <yuxin@Zhous-MacBook-Air-2.local>
2022-10-10 00:59:48 +0800

    Resolved issue with Bird images not showing up

---
+++ pages/Birds.js
@@ -0,0 +1,9 @@
+import Page from "../components/Page";
+
+const Birds = () => (
+  <div>
+    <Page name="Birds" />
+  </div>
+);
+
+export default Birds;
--- pages/Test.js
+++
@@ -1,9 +0,0 @@
-import Page from "../components/Page";
-
-const Test = () => (
-  <div>
-    <Page name="Test" />
-  </div>
-);
-
-export default Test;
