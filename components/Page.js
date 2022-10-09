bc35cf367caae91861d9ef4793fac7a9794cf14d
Zhou Yuxin <yuxin@Zhous-MacBook-Air-2.local>
2022-10-10 00:36:02 +0800

    Fixed Image issue

--- components/Page.js
+++ components/Page.js
@@ -8,8 +8,8 @@ const Page = ({ name = "" }) => (
   <div>
     <div className="mx-auto min-h-screen max-w-6xl py-12">
       <div className="flex gap-7 justify-center">
-        {Object.keys(data[0]).map((pages) => (
-          <NavButton key={pages} title={pages} link={"/".concat(pages)} />
+        {Object.keys(data[0]).map((pages, index) => (
+          <NavButton key={index} title={pages} link={"/".concat(pages)} />
         ))}
       </div>
       <div className="text-center py-20 text-5xl font-bold text-[#000033]">
